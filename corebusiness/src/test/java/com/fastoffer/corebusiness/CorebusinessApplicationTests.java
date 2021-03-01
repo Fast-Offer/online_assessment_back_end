d911b6a26eab15f8df2cf8ab574e93a7973a3f74
Elton-Ge <61650299+Elton-Ge@users.noreply.github.com>
2021-03-01 22:42:41 +1100

    Feature (#2)
    * infrastructure
    
    * infrastructure
    
    * infrastructure
    
    * infrastructure
    
    * infrastructure
    
    * update application.yml

---
+++ corebusiness/src/test/java/com/fastoffer/corebusiness/CorebusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastoffer.corebusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorebusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
