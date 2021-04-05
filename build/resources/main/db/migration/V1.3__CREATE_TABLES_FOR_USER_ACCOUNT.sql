08199c023f5b561c29f591a89083a1d0902b0aa2
Weixin Qi <s4590288@gmail.com>
2021-04-05 22:11:32 +1000

    fix the gitignore problem

--- build/resources/main/db/migration/V1.3__CREATE_TABLES_FOR_USER_ACCOUNT.sql
+++
@@ -1,8 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-DROP TABLE IF EXISTS "user_account";
-
-CREATE TABLE user_account (
-    Id  uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
-    password VARCHAR(99) NOT NULL,
-    email VARCHAR(50) NOT NULL
-)
