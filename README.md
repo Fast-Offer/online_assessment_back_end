9e9a3d6f8cb4688e0723128aea8f93cda3250699
Weixin Qi <s4590288@gmail.com>
2021-04-05 16:28:49 +1000

    Untracked files issue resolved to fix .gitignore

--- README.md
+++
@@ -1,4 +0,0 @@
-# online_assessment_back_end
-
- * run docker-compose up
- * then run BackendApplication
--- src/main/java/com/fastoffer/backend/dtos/LoginPostDto.java
+++
@@ -1,9 +0,0 @@
-package com.fastoffer.backend.dtos;
-
-import lombok.Data;
-
-@Data
-public class LoginPostDto {
-    private String email;
-    private String password;
-}
