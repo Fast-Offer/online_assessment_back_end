cd38e42707b65af58326811b6e71e2549c63ee10
jiahuiyang0@gmail.com <jiahuiyang0@gmail.com>
2021-04-11 12:02:33 +1000

    dev remote

--- src/main/java/com/fastoffer/backend/dtos/Redo/RedoGetDto.java
+++ src/main/java/com/fastoffer/backend/dtos/Redo/RedoGetDto.java
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD:src/main/java/com/fastoffer/backend/dtos/RedoGetDto.java
-package com.fastoffer.backend.dtos;
-
-import lombok.Data;
-
-import java.util.UUID;
-@Data
-public class RedoGetDto {
-    private String id;
-    private String title;
-    private String category;
-    private String status;
-}
-=======
-=======
-<<<<<<<< HEAD:src/main/java/com/fastoffer/backend/dtos/Redo/RedoGetDto.java
->>>>>>> f5975b90ea17aa450cd1e1ace951dbe6d67b6999
 package com.fastoffer.backend.dtos.Redo;
 
 import lombok.Data;
@@ -27,21 +9,3 @@ public class RedoGetDto {
     private String title;
     private String category;
 }
-<<<<<<< HEAD
->>>>>>> f5975b90ea17aa450cd1e1ace951dbe6d67b6999:src/main/java/com/fastoffer/backend/dtos/Redo/RedoGetDto.java
-=======
-========
-package com.fastoffer.backend.dtos;
-
-import lombok.Data;
-
-import java.util.UUID;
-@Data
-public class RedoGetDto {
-    private String id;
-    private String title;
-    private String category;
-    private String status;
-}
->>>>>>>> f5975b90ea17aa450cd1e1ace951dbe6d67b6999:src/main/java/com/fastoffer/backend/dtos/RedoGetDto.java
->>>>>>> f5975b90ea17aa450cd1e1ace951dbe6d67b6999
