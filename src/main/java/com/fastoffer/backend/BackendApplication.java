c2ce79187003302ccbb8ad524afef2d117730754
jiahuiyang0@gmail.com <jiahuiyang0@gmail.com>
2021-04-18 12:47:48 +1000

    fix profile database mapping

--- src/main/java/com/fastoffer/backend/BackendApplication.java
+++ src/main/java/com/fastoffer/backend/BackendApplication.java
@@ -18,19 +18,18 @@ public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
-    @Bean
-    CommandLineRunner commandLineRunner(IntervieweeAccountRepository intervieweeAccountRepository, IntervieweeProfileRepository intervieweeProfileRepository) {
-        return args -> {
-//            intervieweeAccountRepository.findById(UUID.fromString("1b374a97-c04d-43d0-9532-939bcabbdbc5"))
-//                    .ifPresent(System.out::println);
-            intervieweeProfileRepository.findById(UUID.fromString("966b5ba1-5ae4-4461-a581-1d2a116ee2b4"))
-                    .ifPresent(ip -> {
-                        IntervieweeAccountEntity intervieweeAccountEntity = ip.getIntervieweeAccountEntity();
-                        System.out.println("Fetch Lazy...");
-                        System.out.println(intervieweeAccountEntity);
-                    });
-        };
-    }
+//
+//    @Bean
+//    CommandLineRunner commandLineRunner(IntervieweeAccountRepository intervieweeAccountRepository, IntervieweeProfileRepository intervieweeProfileRepository) {
+//        return args -> {
+////            intervieweeAccountRepository.findById(UUID.fromString("1b374a97-c04d-43d0-9532-939bcabbdbc5"))
+////                    .ifPresent(System.out::println);
+//            intervieweeProfileRepository.findById(UUID.fromString("966b5ba1-5ae4-4461-a581-1d2a116ee2b4"))
+//                    .ifPresent(ip -> {
+//                        IntervieweeAccountEntity intervieweeAccountEntity = ip.getIntervieweeAccountEntity();
+//                        System.out.println(intervieweeAccountEntity);
+//                    });
+//        };
+//    }
 
 }
